3f7f2f7767bcad55618f11a7ae9846e73a4c062d
Adminuser <rcquisto@ncsu.edu>
2018-12-11 21:58:39 -0500

    Add models

---
+++ visualnarrativeproj/Assets/TestScripts/Puzzles/Image/ImagePuzzle.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class ImagePuzzle : Singleton<ImagePuzzle>
+{
+    public int speed = 10;
+    public int totalSwaps = 13;
+    public GameObject[] originalArray = new GameObject[6];
+    private Vector3[] startingPositions = new Vector3[6];
+    public GameObject[] orderedArray;
+    public int total;
+    public int chosenIndex = -1;
+    public Boolean doneMoving = true;
+    public Color color = Color.white;
+    // reference to exhibit script
+    public GameObject exhibit;
+
+    void Start()
+    {
+        orderedArray = (GameObject[])originalArray.Clone();
+        total = orderedArray.Length;
+        UpdateStartingPositions();
+        color.a = 0.66f;
+        StartCoroutine(RandomizeOrder());
+        Awake();
+    }
+
+    void UpdateStartingPositions()
+    {
+        for (int i = 0; i < total; i++)
+        {
+            startingPositions[i] = orderedArray[i].transform.position;
+        }
+    }
+
+    IEnumerator RandomizeOrder()
+    {
+        for (int i = 0; i < totalSwaps; i++)
+        {
+            while (!doneMoving)
+            {
+                yield return null;
+            }
+            StartCoroutine(SwapPositions(UnityEngine.Random.Range(0, total), UnityEngine.Random.Range(0, total), true));
+        }
+    }
+
+    public IEnumerator SwapPositions(int a, int b, Boolean isStarting = false)
+    {
+        //To prevent the swap before another one is finished
+        doneMoving = false;
+        while (Vector3.Distance(orderedArray[a].transform.position, startingPositions[b]) > 0.01f ||
+               Vector3.Distance(orderedArray[b].transform.position, startingPositions[a]) > 0.01f)
+        {
+            orderedArray[a].transform.position = Vector3.MoveTowards(orderedArray[a].transform.position, startingPositions[b], speed * Time.deltaTime);
+            orderedArray[b].transform.position = Vector3.MoveTowards(orderedArray[b].transform.position, startingPositions[a], speed * Time.deltaTime);
+            yield return null;
+        }
+
+        orderedArray[a].transform.position = startingPositions[b];
+        orderedArray[b].transform.position = startingPositions[a];
+
+        GameObject temp = orderedArray[a];
+        orderedArray[a] = orderedArray[b];
+        orderedArray[b] = temp;
+
+        UpdateStartingPositions();
+        doneMoving = isStarting || CheckComplete();
+    }
+
+    Boolean CheckComplete()
+    {
+        for (int i = 0; i < total; i++)
+        {
+            if (!originalArray[i].name.Equals(orderedArray[i].name))
+            {
+                return true;
+            }
+        }
+        orderedArray[chosenIndex].transform.GetChild(0).GetComponent<SpriteRenderer>().color = Color.white;
+        chosenIndex = -1;
+        return false;
+    }
+
+    void Update()
+    {
+        // If the exhibit is not active do not accept user input
+        if (!exhibit.GetComponent<S_Exhibit>().getIsActive())
+            return;
+    }
+}
---
+++ visualnarrativeproj/Assets/TestScripts/Puzzles/Image/Tile.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System;
+using VRTK;
+
+public class Tile : VRTK_InteractableObject
+{
+    public GameObject obj;
+    private int index = -1;
+
+    void Start()
+    {
+        //
+    }
+
+
+    public override void StartUsing(VRTK_InteractUse currentUsingObject = null)
+    {
+        base.StartUsing(currentUsingObject);
+        if (ImagePuzzle.Instance.doneMoving)
+        {
+            if (Array.IndexOf(ImagePuzzle.Instance.orderedArray, obj) > -1)
+            {
+                index = Array.IndexOf(ImagePuzzle.Instance.orderedArray, obj);
+                if (ImagePuzzle.Instance.chosenIndex != -1)
+                {
+                    ImagePuzzle.Instance.orderedArray[ImagePuzzle.Instance.chosenIndex].transform.GetChild(0).GetComponent<SpriteRenderer>().color = Color.white;
+                }
+                if (index != ImagePuzzle.Instance.chosenIndex)
+                {
+                    ImagePuzzle.Instance.orderedArray[index].transform.GetChild(0).GetComponent<SpriteRenderer>().color = ImagePuzzle.Instance.color;
+                    ImagePuzzle.Instance.chosenIndex = index;
+                }
+                else
+                {
+                    ImagePuzzle.Instance.chosenIndex = -1;
+                }
+            }
+        }
+    }
+
+    public override void StopUsing(VRTK_InteractUse previousUsingObject = null, bool resetUsingObjectState = true)
+    {
+        base.StopUsing(previousUsingObject, resetUsingObjectState);
+        ImagePuzzle.Instance.orderedArray[Array.IndexOf(ImagePuzzle.Instance.orderedArray, obj)].transform.GetChild(0).GetComponent<SpriteRenderer>().color = Color.white;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        if (ImagePuzzle.Instance.doneMoving && ImagePuzzle.Instance.chosenIndex >= 0)
+        {
+            if (OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger) > 0.9 && ImagePuzzle.Instance.chosenIndex < ImagePuzzle.Instance.total - 1)
+            {
+                StartCoroutine(ImagePuzzle.Instance.SwapPositions(ImagePuzzle.Instance.chosenIndex++, ImagePuzzle.Instance.chosenIndex));
+            }
+
+            if (OVRInput.Get(OVRInput.Axis1D.SecondaryHandTrigger) > 0.9 && ImagePuzzle.Instance.chosenIndex > 0)
+            {
+                StartCoroutine(ImagePuzzle.Instance.SwapPositions(ImagePuzzle.Instance.chosenIndex--, ImagePuzzle.Instance.chosenIndex));
+            }
+        }
+    }
+}
